1ae7f7cfb523d25a46600a1190b148908a97e54c
András Fekete <andris2583@gmail.com>
2025-02-14 13:15:19 +0100

    First question changes

--- week1/1.py
+++ week1/1.py
@@ -14,16 +14,21 @@ def gammaCorrection(image, gamma, c):
 def one():
   image = io.imread("modelhouses.png") / 255.0
 
-  gamma_image = gammaCorrection(image, 3, 1)
+  gamma_image = gammaCorrection(image, 0.5, 1)
 
-  fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+  fig, ax = plt.subplots(1, 3, figsize=(10, 5))
   ax[0].imshow(image, cmap="gray")
   ax[0].set_title("Grayscale image")
   ax[0].axis("off")
 
   ax[1].imshow(gamma_image, cmap="gray")
-  ax[1].set_title("Gamma corrected grayscale image")
+  ax[1].set_title("Gamma = 0.5")
   ax[1].axis("off")
+  
+  gamma_image_2 = gammaCorrection(image, 2, 1)
+  ax[2].imshow(gamma_image_2, cmap="gray")
+  ax[2].set_title("Gamma = 2")
+  ax[2].axis("off")
 
   plt.show()
 
@@ -34,9 +39,9 @@ def two():
   green = image[:, :, 1]
   blue = image[:, :, 2]
   
-  red_gamma = gammaCorrection(red, 0.5, 1)
-  green_gamma = gammaCorrection(green, 0.5, 1)
-  blue_gamma = gammaCorrection(blue, 0.5, 1)
+  red_gamma = gammaCorrection(red, 0.8, 1)
+  green_gamma = gammaCorrection(green, 0.8, 1)
+  blue_gamma = gammaCorrection(blue, 0.8, 1)
 
   gamma_image = np.dstack((red_gamma,green_gamma, blue_gamma))
   print(gamma_image)
@@ -58,7 +63,7 @@ def three():
 
   value = hsv_image[:, :, 2]
 
-  value_gamma = gammaCorrection(value, 0.5, 1)
+  value_gamma = gammaCorrection(value, 0.8, 1)
 
   gamma_hsv_image = np.dstack((hsv_image[:, :, 0],hsv_image[:, :, 1], value_gamma))
   gamma_rgb_image = hsv2rgb(gamma_hsv_image)

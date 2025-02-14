2de2dec78c424c4db58b6d788b36684eb73c7331
András Fekete <andris2583@gmail.com>
2025-02-14 13:43:13 +0100

    Question 4.1 and 4.2

---
+++ week1/4.py
@@ -0,0 +1,48 @@
+import numpy as np
+from skimage import data
+from skimage.color import rgb2hsv, hsv2rgb
+import matplotlib.pyplot as plt
+from skimage import io, color
+from scipy.ndimage import gaussian_filter
+
+
+def one():
+  image = io.imread("pout.tif") / 255.0
+  
+  bin_counts, bin_edges = np.histogram(image, 256)
+
+  cdf = np.cumsum(bin_counts)
+  cdf = cdf / cdf[-1]
+  plt.plot(cdf, color='blue')
+  plt.show()
+  
+
+def cdfTransform(image, cdf):
+    image = (image * 255).astype(np.uint8)
+
+    transformed_image = cdf[image]
+
+    return transformed_image
+
+def two():
+  image = io.imread("pout.tif") / 255.0
+  
+  histogram, _ = np.histogram(image, bins=256, range=[0,1])
+
+  cdf = np.cumsum(histogram)
+  cdf = cdf / cdf[-1]
+
+  transformed_image = cdfTransform(image, cdf)
+
+  fig, ax = plt.subplots(1, 2, figsize=(12, 5))
+  ax[0].imshow(image, cmap="gray")
+  ax[0].set_title("Original Image")
+  ax[0].axis("off")
+
+  ax[1].imshow(transformed_image, cmap="gray")
+  ax[1].set_title("Transformed using CDF")
+  ax[1].axis("off")
+
+  plt.show()
+
+two()
